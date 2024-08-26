d8c3e242b9d5501f8da9277af54bbb50929e854b
Abishek-GH <abishek0149@gmail.com>
2024-08-26 10:29:09 +0530

    Day 1 Update

--- Day_1.py
+++ Day_1.py
@@ -9,7 +9,7 @@ numbers = list(map(int, input_data.split()))
 
 # Calculate the sum of the numbers
 total_sum = sum(numbers)
-
+print("Hello")
 # Write the output to the output.txt file
 with open('output.txt', 'w') as file:
     file.write(f"The sum of the numbers is: {total_sum}")
